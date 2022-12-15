0599dc47c9199cc92ee9b1ccffbcbf0ee35dc986
Shirasawa <764798966@qq.com>
2022-12-15 23:38:14 +0800

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,20 @@
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.compose.ExperimentalComposeLibrary
 import java.net.URL
 import java.net.HttpURLConnection
 import java.io.FileOutputStream
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("multiplatform")
     id("org.jetbrains.compose")
-    kotlin("plugin.serialization") version "1.7.20"
+    kotlin("plugin.serialization") version "1.7.21"
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.eimsound"
-version = "1.0-SNAPSHOT"
+version = "0.0.0"
 
 repositories {
     google()
@@ -22,10 +25,10 @@ repositories {
 }
 
 kotlin {
-    jvmToolchain(17)
+    jvmToolchain(18)
     jvm {
         compilations.all {
-            kotlinOptions.jvmTarget = "17"
+            kotlinOptions.jvmTarget = "18"
         }
         withJava()
     }
@@ -76,10 +79,25 @@ task<Copy>("downloadEIMHost") {
 }
 
 // Run before build
-tasks.withType<GradleBuild>() {
+tasks.withType<GradleBuild> {
     dependsOn(":downloadEIMHost")
 }
 
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-    kotlinOptions.languageVersion = "1.8"
+tasks.withType<KotlinCompile>().configureEach {
+    kotlinOptions.languageVersion = "1.9"
+}
+
+tasks.withType<Jar> {
+    manifest {
+        attributes(
+            "Name" to "EchoInMirror",
+            "Main-Class" to "cn.apisium.eim.MainKt",
+            "Version" to project.version,
+        )
+    }
+}
+
+tasks.withType<ShadowJar> {
+    archiveClassifier.set("")
+    archiveVersion.set("")
 }
