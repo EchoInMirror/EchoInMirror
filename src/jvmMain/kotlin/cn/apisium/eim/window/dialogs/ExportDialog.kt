12ca02757f145e2c4cbfb1c0afe6fcc511c69127
N0I0C0K <nick131410@aliyun.com>
2023-01-04 23:35:16 +0800

    修正了布局, 删除了错误用法

--- src/jvmMain/kotlin/cn/apisium/eim/window/dialogs/ExportDialog.kt
+++ src/jvmMain/kotlin/cn/apisium/eim/window/dialogs/ExportDialog.kt
@@ -6,17 +6,13 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExpandMore
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.window.Dialog
 import cn.apisium.eim.EchoInMirror
 import cn.apisium.eim.impl.RendererImpl
 import java.awt.Dimension
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.zIndex
@@ -33,10 +29,11 @@ private fun closeQuickLoadWindow() {
 
 data class ExportFormate(val extend: String, val isLossLess: Boolean = true, val formate: AudioFileFormat.Type? = null)
 
+private val floatingDialogProvider = FloatingDialogProvider()
+
 @Composable
 private fun Menu(
     menuItems: @Composable (closeDialog: () -> Unit) -> Unit,
-    floatingDialogProvider: FloatingDialogProvider? = null,
     content: @Composable () -> Unit
 ) {
     FloatingDialog({ _, close ->
@@ -49,7 +46,7 @@ private fun Menu(
                 menuItems(close)
             }
         }
-    }, modifier = Modifier.zIndex(100f), floatingDialogProvider = floatingDialogProvider) {
+    }, modifier = Modifier.zIndex(100f)) {
         Surface(
             shadowElevation = 2.dp,
             tonalElevation = 4.dp
@@ -71,196 +68,198 @@ val ExportDialog = @Composable {
     Dialog(::closeQuickLoadWindow, title = "导出") {
         window.minimumSize = Dimension(300, 300)
         window.isModal = false
-        val floatingDialogProvider = remember { FloatingDialogProvider() }
-        Surface(Modifier.fillMaxSize(), tonalElevation = 4.dp) {
-            floatingDialogProvider.FloatingDialogs()
-            Box(Modifier.padding(10.dp)) {
-                Column {
-                    Row {
-                        val position = EchoInMirror.currentPosition
-                        val endPPQ = position.projectRange.last - position.projectRange.first
-                        Text("长度:")
-                        Text(
-                            "${endPPQ / position.ppq / position.timeSigNumerator + 1}节",
-                            color = Color.Black.copy(0.5f)
-                        )
-                        Spacer(Modifier.width(10.dp))
-                        val timeInSecond = position.convertPPQToSamples(endPPQ).toFloat() / position.sampleRate
-                        Text(
-                            "总时间:"
-                        )
-                        Text(
-                            "${
-                                (timeInSecond / 60).toInt().toString().padStart(2, '0')
-                            }'${(timeInSecond % 60).toInt().toString().padStart(2, '0')}\"",
-                            color = Color.Black.copy(0.5f)
-                        )
-                        Spacer(Modifier.width(10.dp))
-                    }
-
-                    Spacer(Modifier.height(10.dp))
-                    Divider()
-                    Spacer(Modifier.height(10.dp))
-                    val exportOptions = listOf<ExportFormate>(
-                        ExportFormate("wav", formate = AudioFileFormat.Type.WAVE),
-                        ExportFormate("au", formate = AudioFileFormat.Type.AU),
-                        ExportFormate("aif", formate = AudioFileFormat.Type.AIFF),
-                        ExportFormate("aifc", formate = AudioFileFormat.Type.AIFC),
-                        ExportFormate("snd", formate = AudioFileFormat.Type.SND),
-                        ExportFormate("flac"),
-                        ExportFormate("mp3", false),
-                        ExportFormate("ogg", false)
-                    )
-                    var exportSelect by remember { mutableStateOf(exportOptions[0]) }
-                    Row {
+        CompositionLocalProvider(LocalFloatingDialogProvider.provides(floatingDialogProvider)) {
+            Surface(Modifier.fillMaxSize(), tonalElevation = 4.dp) {
+                floatingDialogProvider.FloatingDialogs()
+                Box(Modifier.padding(10.dp)) {
+                    Column {
                         Row {
-                            Text("导出格式 ", Modifier.width(80.dp))
-                            Menu({ close ->
-                                exportOptions.map {
-                                    MenuItem(
-                                        exportSelect == it, {
-                                            close()
-                                            exportSelect = it
-                                        }
-                                    ) {
-                                        Text(it.extend)
-                                    }
-                                }
-                            }, floatingDialogProvider) {
-                                Text(
-                                    exportSelect.extend
-                                )
-                            }
+                            val position = EchoInMirror.currentPosition
+                            val endPPQ = position.projectRange.last - position.projectRange.first
+                            Text("长度:")
+                            Text(
+                                "${endPPQ / position.ppq / position.timeSigNumerator + 1}节",
+                                color = Color.Black.copy(0.5f)
+                            )
+                            Spacer(Modifier.width(10.dp))
+                            val timeInSecond = position.convertPPQToSamples(endPPQ).toFloat() / position.sampleRate
+                            Text(
+                                "总时间:"
+                            )
+                            Text(
+                                "${
+                                    (timeInSecond / 60).toInt().toString().padStart(2, '0')
+                                }'${(timeInSecond % 60).toInt().toString().padStart(2, '0')}\"",
+                                color = Color.Black.copy(0.5f)
+                            )
+                            Spacer(Modifier.width(10.dp))
                         }
 
-                        Spacer(Modifier.width(10.dp))
-
-                        val soundOptions = listOf<String>("立体声", "左声道", "右声道")
-                        var soundSelect by remember { mutableStateOf(soundOptions[0]) }
+                        Spacer(Modifier.height(10.dp))
+                        Divider()
+                        Spacer(Modifier.height(10.dp))
+                        val exportOptions = listOf<ExportFormate>(
+                            ExportFormate("wav", formate = AudioFileFormat.Type.WAVE),
+                            ExportFormate("au", formate = AudioFileFormat.Type.AU),
+                            ExportFormate("aif", formate = AudioFileFormat.Type.AIFF),
+                            ExportFormate("aifc", formate = AudioFileFormat.Type.AIFC),
+                            ExportFormate("snd", formate = AudioFileFormat.Type.SND),
+                            ExportFormate("flac"),
+                            ExportFormate("mp3", false),
+                            ExportFormate("ogg", false)
+                        )
+                        var exportSelect by remember { mutableStateOf(exportOptions[0]) }
                         Row {
-                            Menu({ close ->
-                                Column {
-                                    soundOptions.map {
+                            Row {
+                                Text("导出格式 ", Modifier.width(80.dp))
+                                Menu({ close ->
+                                    exportOptions.map {
                                         MenuItem(
-                                            soundSelect == it, {
+                                            exportSelect == it, {
                                                 close()
-                                                soundSelect = it
+                                                exportSelect = it
                                             }
                                         ) {
-                                            Text(it)
+                                            Text(it.extend)
                                         }
                                     }
-                                }
-                            }, floatingDialogProvider) {
-                                Text(soundSelect)
-                            }
-                        }
-                    }
-
-                    Spacer(Modifier.height(5.dp))
-
-                    Row {
-                        if (exportSelect.isLossLess) {
-                            val deepList = listOf<Int>(32, 24, 16)
-                            var deep by remember { mutableStateOf(deepList[0]) }
-                            Row {
-                                Text("位深 ", Modifier.width(80.dp))
-                                Menu({ close ->
-                                    Column {
-                                        deepList.map {
-                                            MenuItem(
-                                                deep == it, {
-                                                    close()
-                                                    deep = it
-                                                }
-                                            ) {
-                                                Text(it.toString())
-                                            }
-                                        }
-                                    }
-                                }, floatingDialogProvider) {
-                                    Text("${deep}位")
+                                }) {
+                                    Text(
+                                        exportSelect.extend
+                                    )
                                 }
                             }
-                        }
 
-                        if (!exportSelect.isLossLess) {
-                            val bitRateOptions = listOf<Int>(128, 192, 256, 320)
-                            var bitRate by remember { mutableStateOf(bitRateOptions[0]) }
-                            Row {
-                                Text("比特率 ", Modifier.width(80.dp))
-                                Menu({ close ->
-                                    Column {
-                                        bitRateOptions.map {
-                                            MenuItem(
-                                                bitRate == it, {
-                                                    close()
-                                                    bitRate = it
-                                                }
-                                            ) {
-                                                Text(it.toString())
-                                            }
-                                        }
-                                    }
-                                }, floatingDialogProvider) {
-                                    Text("${bitRate}kbps")
-                                }
-                            }
-                        }
-
-                        if (exportSelect.extend == "flac") {
                             Spacer(Modifier.width(10.dp))
-                            val flacCompressOptions = 0..8
-                            var flacCompress by remember { mutableStateOf(5) }
+
+                            val soundOptions = listOf<String>("立体声", "左声道", "右声道")
+                            var soundSelect by remember { mutableStateOf(soundOptions[0]) }
                             Row {
-                                Text("flac压缩 ", Modifier.width(80.dp))
                                 Menu({ close ->
                                     Column {
-                                        flacCompressOptions.map {
+                                        soundOptions.map {
                                             MenuItem(
-                                                flacCompress == it, {
+                                                soundSelect == it, {
                                                     close()
-                                                    flacCompress = it
+                                                    soundSelect = it
                                                 }
                                             ) {
-                                                Text(it.toString())
+                                                Text(it)
                                             }
                                         }
                                     }
-                                }, floatingDialogProvider) {
-                                    Text(flacCompress.toString())
+                                }) {
+                                    Text(soundSelect)
                                 }
                             }
                         }
-                    }
-                    Filled()
-                    Button(onClick = {
-                        val renderer = EchoInMirror.bus?.let { RendererImpl(it) }
-                        val position = EchoInMirror.currentPosition
-                        val audioFile = File("./test.wav")
-                        EchoInMirror.player!!.close()
-                        GlobalScope.launch {
-                            renderer?.start(
-                                position.projectRange,
-                                position.sampleRate,
-                                position.ppq,
-                                position.bpm,
-                                audioFile,
-                                AudioFileFormat.Type.WAVE
-                            ) {
-                                println(it)
+
+                        Spacer(Modifier.height(5.dp))
+
+                        Row {
+                            if (exportSelect.isLossLess) {
+                                val deepList = listOf<Int>(32, 24, 16)
+                                var deep by remember { mutableStateOf(deepList[0]) }
+                                Row {
+                                    Text("位深 ", Modifier.width(80.dp))
+                                    Menu({ close ->
+                                        Column {
+                                            deepList.map {
+                                                MenuItem(
+                                                    deep == it, {
+                                                        close()
+                                                        deep = it
+                                                    }
+                                                ) {
+                                                    Text(it.toString())
+                                                }
+                                            }
+                                        }
+                                    }) {
+                                        Text("${deep}位")
+                                    }
+                                }
+                            }
+
+                            if (!exportSelect.isLossLess) {
+                                val bitRateOptions = listOf<Int>(128, 192, 256, 320)
+                                var bitRate by remember { mutableStateOf(bitRateOptions[0]) }
+                                Row {
+                                    Text("比特率 ", Modifier.width(80.dp))
+                                    Menu({ close ->
+                                        Column {
+                                            bitRateOptions.map {
+                                                MenuItem(
+                                                    bitRate == it, {
+                                                        close()
+                                                        bitRate = it
+                                                    }
+                                                ) {
+                                                    Text(it.toString())
+                                                }
+                                            }
+                                        }
+                                    }) {
+                                        Text("${bitRate}kbps")
+                                    }
+                                }
+                            }
+
+                            if (exportSelect.extend == "flac") {
+                                Spacer(Modifier.width(10.dp))
+                                val flacCompressOptions = 0..8
+                                var flacCompress by remember { mutableStateOf(5) }
+                                Row {
+                                    Text("flac压缩 ", Modifier.width(80.dp))
+                                    Menu({ close ->
+                                        Column {
+                                            flacCompressOptions.map {
+                                                MenuItem(
+                                                    flacCompress == it, {
+                                                        close()
+                                                        flacCompress = it
+                                                    }
+                                                ) {
+                                                    Text(it.toString())
+                                                }
+                                            }
+                                        }
+                                    }) {
+                                        Text(flacCompress.toString())
+                                    }
+                                }
                             }
                         }
-                    }, Modifier.zIndex(-10f).fillMaxWidth()) {
-                        Row {
-                            Text("导出到 test.${exportSelect.extend}")
+                        Filled()
+                        Button(onClick = {
+                            val renderer = EchoInMirror.bus?.let { RendererImpl(it) }
+                            val position = EchoInMirror.currentPosition
+                            val audioFile = File("./test.wav")
+                            EchoInMirror.player!!.close()
+                            GlobalScope.launch {
+                                renderer?.start(
+                                    position.projectRange,
+                                    position.sampleRate,
+                                    position.ppq,
+                                    position.bpm,
+                                    audioFile,
+                                    AudioFileFormat.Type.WAVE
+                                ) {
+                                    println(it)
+                                }
+                            }
+                        }, Modifier.zIndex(-10f).fillMaxWidth()) {
+                            Row {
+                                Text("导出到 test.${exportSelect.extend}")
+                            }
                         }
-                    }
 
+
+                    }
 
                 }
-
             }
         }
+
     }
 }
