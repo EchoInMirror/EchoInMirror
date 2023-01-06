7c5daa07799a7583505bba134e973cf6d6631bae
Shirasawa <764798966@qq.com>
2023-01-07 00:57:44 +0800

    Update

--- src/jvmMain/kotlin/cn/apisium/eim/window/panels/Mixer.kt
+++ src/jvmMain/kotlin/cn/apisium/eim/window/panels/Mixer.kt
@@ -12,12 +12,10 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.layout.onGloballyPositioned
-import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.layout.Layout
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
@@ -43,106 +41,122 @@ fun MixerProcessorButton(title: String, useMarquee: Boolean = false, fontStyle: 
 }
 
 @Composable
-private fun MixerTrack(track: Track, index: String, height: MutableState<Dp>?, modifier: Modifier = Modifier,
-                       isLastTrack: Boolean = false, drawSplitter: Boolean = true, isRound: Boolean = false,
-                       renderChildren: Boolean = true) {
-    Row(if (isRound) modifier.shadow(1.dp, MaterialTheme.shapes.medium, clip = false)
-        .background(MaterialTheme.colorScheme.surface, MaterialTheme.shapes.medium).clip(MaterialTheme.shapes.medium)
-    else modifier) {
+private fun MixerTrack(track: Track, index: String, depth: Int = 0, renderChildren: Boolean = true) {
+    Row(Modifier.padding(7.dp, 14.dp, 7.dp, 14.dp)
+        .shadow(1.dp, MaterialTheme.shapes.medium, clip = false)
+        .background(MaterialTheme.colorScheme.surfaceColorAtElevation(((depth + 2) * 2).dp))
+        .clip(MaterialTheme.shapes.medium)) {
         val trackColor = if (track is Bus) MaterialTheme.colorScheme.primary else track.color
-        val selectedTrack = EchoInMirror.selectedTrack
+        val isSelected = EchoInMirror.selectedTrack == track
         var curModifier = Modifier.width(80.dp)
-        if (selectedTrack == track && height != null) curModifier = curModifier.height(height.value)
-            .border(1.dp, trackColor, RoundedCornerShape(
-                CornerSize(0.dp), CornerSize(0.dp), MaterialTheme.shapes.medium.bottomEnd, MaterialTheme.shapes.medium.bottomStart)
-            )
-        else if (drawSplitter) curModifier = curModifier.border(start = Border(1.dp, MaterialTheme.colorScheme.outlineVariant))
-        Column(curModifier) {
-            Row(Modifier.background(trackColor).clickableWithIcon { if (track != EchoInMirror.bus) EchoInMirror.selectedTrack = track }
-                .padding(vertical = 2.5.dp).zIndex(2f)) {
-                val color = trackColor.toOnSurfaceColor()
-                Text(
-                    index,
-                    Modifier.padding(start = 5.dp, end = 3.dp),
-                    color = color,
-                    fontSize = MaterialTheme.typography.labelLarge.fontSize,
-                    lineHeight = 18.0.sp,
-                    fontWeight = FontWeight.Bold
-                )
-                Marquee {
+        if (isSelected) curModifier = curModifier.border(1.dp, trackColor, RoundedCornerShape(
+            CornerSize(0.dp), CornerSize(0.dp), MaterialTheme.shapes.medium.bottomEnd, MaterialTheme.shapes.medium.bottomStart)
+        )
+
+        Layout({
+            Column(curModifier.shadow(1.dp, MaterialTheme.shapes.medium, clip = false)
+                .background(MaterialTheme.colorScheme.surface, MaterialTheme.shapes.medium)
+                .clip(MaterialTheme.shapes.medium)) {
+                Row(Modifier.background(trackColor).height(24.dp)
+                    .clickableWithIcon { if (track != EchoInMirror.bus) EchoInMirror.selectedTrack = track }
+                    .padding(vertical = 2.5.dp).zIndex(2f)) {
+                    val color = trackColor.toOnSurfaceColor()
                     Text(
-                        track.name,
+                        index,
+                        Modifier.padding(start = 5.dp, end = 3.dp),
                         color = color,
                         fontSize = MaterialTheme.typography.labelLarge.fontSize,
                         lineHeight = 18.0.sp,
+                        fontWeight = FontWeight.Bold
                     )
+                    Marquee {
+                        Text(
+                            track.name,
+                            color = color,
+                            fontSize = MaterialTheme.typography.labelLarge.fontSize,
+                            lineHeight = 18.0.sp,
+                        )
+                    }
                 }
-            }
 
-            Column(Modifier.padding(4.dp)) {
-                Slider(
-                    track.pan,
-                    { track.pan = it },
-                    valueRange = -1f..1f,
-                    modifier = Modifier.fillMaxWidth(),
-                    track = { modifier, progress, interactionSource, tickFractions, enabled, isVertical ->
-                        DefaultTrack(modifier, progress, interactionSource, tickFractions, enabled, isVertical, startPoint = 0.5f)
-                    }
-                )
-
-                Row(
-                    Modifier.fillMaxWidth().padding(vertical = 4.dp),
-                    horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically
-                ) {
-                    IconButton({ track.isMute = !track.isMute }, Modifier.size(20.dp)) {
-                        if (track.isMute) Icon(Icons.Default.VolumeOff, null, tint = MaterialTheme.colorScheme.error)
-                        else Icon(Icons.Default.VolumeUp, null)
-                    }
-                    Text(
-                        track.levelMeter.cachedMaxLevelString,
-                        Modifier.width(30.dp).padding(start = 2.dp),
-                        fontSize = MaterialTheme.typography.labelMedium.fontSize,
-                        textAlign = TextAlign.Center,
-                        letterSpacing = (-1).sp,
-                        lineHeight = 12.sp,
-                        maxLines = 1
+                Column(Modifier.padding(4.dp)) {
+                    Slider(
+                        track.pan,
+                        { track.pan = it },
+                        valueRange = -1f..1f,
+                        modifier = Modifier.fillMaxWidth(),
+                        track = { modifier, progress, interactionSource, tickFractions, enabled, isVertical ->
+                            DefaultTrack(modifier, progress, interactionSource, tickFractions, enabled, isVertical, startPoint = 0.5f)
+                        }
                     )
+
+                    Row(
+                        Modifier.fillMaxWidth().padding(vertical = 4.dp),
+                        horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        IconButton({ track.isMute = !track.isMute }, Modifier.size(20.dp)) {
+                            if (track.isMute) Icon(Icons.Default.VolumeOff, null, tint = MaterialTheme.colorScheme.error)
+                            else Icon(Icons.Default.VolumeUp, null)
+                        }
+                        Text(
+                            track.levelMeter.cachedMaxLevelString,
+                            Modifier.width(30.dp).padding(start = 2.dp),
+                            fontSize = MaterialTheme.typography.labelMedium.fontSize,
+                            textAlign = TextAlign.Center,
+                            letterSpacing = (-1).sp,
+                            lineHeight = 12.sp,
+                            maxLines = 1
+                        )
+                    }
+
+                    Row(
+                        Modifier.fillMaxWidth().padding(bottom = 2.dp),
+                        horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        VolumeSlider(track)
+                        Gap(18)
+                        Level(track.levelMeter, Modifier.height(136.dp))
+                    }
                 }
 
-                Row(
-                    Modifier.fillMaxWidth().padding(bottom = 2.dp),
-                    horizontalArrangement = Arrangement.Center, verticalAlignment = Alignment.CenterVertically
-                ) {
-                    VolumeSlider(track)
-                    Gap(18)
-                    Level(track.levelMeter, Modifier.height(136.dp))
+                Column {
+                    Divider(Modifier.padding(horizontal = 8.dp))
+                    track.preProcessorsChain.forEach {
+                        MixerProcessorButton(it.name, true, onClick = it::onClick)
+                        Divider(Modifier.padding(horizontal = 8.dp))
+                    }
+                    MixerProcessorButton("...", fontWeight = FontWeight.Bold) { }
+                    Divider(Modifier.padding(horizontal = 6.dp), 2.dp, MaterialTheme.colorScheme.primary)
+                    track.postProcessorsChain.forEach {
+                        MixerProcessorButton(it.name, true, onClick = it::onClick)
+                        Divider(Modifier.padding(horizontal = 8.dp))
+                    }
+                    MixerProcessorButton("...", fontWeight = FontWeight.Bold) { }
                 }
             }
-
-            Column {
-                Divider(Modifier.padding(horizontal = 8.dp))
-                track.preProcessorsChain.forEach {
-                    MixerProcessorButton(it.name, true, onClick = it::onClick)
-                    Divider(Modifier.padding(horizontal = 8.dp))
+            if (renderChildren && track.subTracks.isNotEmpty()) {
+                Row(Modifier.padding(horizontal = 7.dp)) {
+                    track.subTracks.forEachIndexed { i, it ->
+                        key(it) {
+                            MixerTrack(it, "$index.${i + 1}", depth + 1)
+                        }
+                    }
                 }
-                MixerProcessorButton("...", fontWeight = FontWeight.Bold) { }
-                Divider(Modifier.padding(horizontal = 6.dp), 2.dp, MaterialTheme.colorScheme.primary)
-                track.postProcessorsChain.forEach {
-                    MixerProcessorButton(it.name, true, onClick = it::onClick)
-                    Divider(Modifier.padding(horizontal = 8.dp))
-                }
-                MixerProcessorButton("...", fontWeight = FontWeight.Bold) { }
             }
-        }
-
-        if (renderChildren && track.subTracks.isNotEmpty()) {
-            var lastTrack = track
-            val size = track.subTracks.size - 1
-            track.subTracks.forEachIndexed { i, it ->
-                key(it) {
-                    MixerTrack(it, "$index.${i + 1}", height = height, isLastTrack = isLastTrack && size == i,
-                        drawSplitter = lastTrack != selectedTrack && it != selectedTrack)
-                    lastTrack = it
+        }) { measurables, constraints ->
+            if (measurables.size > 1) {
+                val content = measurables[1].measure(constraints)
+                var maxHeight = content.height
+                val mixer = measurables[0].measure(constraints.copy(minHeight = maxHeight))
+                if (mixer.height > maxHeight) maxHeight = mixer.height
+                layout(content.width + mixer.width, maxHeight) {
+                    mixer.place(0, 0)
+                    content.place(mixer.width, 0)
+                }
+            } else {
+                val mixer = measurables[0].measure(constraints)
+                layout(mixer.width, mixer.height) {
+                    mixer.place(0, 0)
                 }
             }
         }
@@ -161,18 +175,12 @@ object Mixer: Panel {
     @Composable
     override fun content() {
         Scrollable {
-            Row(Modifier.padding(14.dp)) {
+            Row {
                 val bus = EchoInMirror.bus!!
-                MixerTrack(bus, "0", null, Modifier, isRound = true,
-                    renderChildren = false, drawSplitter = false)
-                val localDensity = LocalDensity.current
+                MixerTrack(bus, "0", renderChildren = false)
                 bus.subTracks.forEachIndexed { i, it ->
                     key(it) {
-                        Gap(8)
-                        val state = remember { mutableStateOf(0.dp) }
-                        MixerTrack(it, (i + 1).toString(), state, Modifier.onGloballyPositioned {
-                            with (localDensity) { state.value = it.size.height.toDp() }
-                        }, isRound = true, drawSplitter = false)
+                        MixerTrack(it, (i + 1).toString())
                     }
                 }
             }
