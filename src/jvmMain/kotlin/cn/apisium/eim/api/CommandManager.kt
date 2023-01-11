f5c7d1051b10ef1291fe6736942de10c7f8a1b94
N0I0C0K <nick131410@aliyun.com>
2023-01-11 22:13:33 +0800

    增加了快捷键窗口, 支持了快捷键的变更

--- src/jvmMain/kotlin/cn/apisium/eim/api/CommandManager.kt
+++ src/jvmMain/kotlin/cn/apisium/eim/api/CommandManager.kt
@@ -4,14 +4,21 @@ import androidx.compose.ui.input.key.Key
 
 interface Command {
     val name: String
-    fun execute() { }
+    val displayName: String
+    fun execute() {}
     val keyBindings: Array<Key>
     val activeWhen: Array<String>?
 }
 
-abstract class AbstractCommand(override val name: String, override val keyBindings: Array<Key>, override val activeWhen: Array<String>? = null): Command
+abstract class AbstractCommand(
+    override val name: String,
+    override val displayName: String,
+    override val keyBindings: Array<Key>,
+    override val activeWhen: Array<String>? = null
+) : Command
 
 interface CommandManager {
+    val commands: Map<String, Command>
     fun registerCommand(command: Command)
     fun registerCommandHandler(command: Command, handler: () -> Unit)
     fun executeCommand(command: String)
--- src/jvmMain/kotlin/cn/apisium/eim/commands/UICommands.kt
+++ src/jvmMain/kotlin/cn/apisium/eim/commands/UICommands.kt
@@ -8,13 +8,13 @@ import cn.apisium.eim.EchoInMirror
 import cn.apisium.eim.api.AbstractCommand
 import cn.apisium.eim.window.dialogs.settings.SettingsWindow
 
-object OpenSettingsCommand: AbstractCommand("EIM:Open Settings", arrayOf(Key.CtrlLeft, Key.Comma)) {
+object OpenSettingsCommand : AbstractCommand("EIM:Open Settings", "打开设置", arrayOf(Key.CtrlLeft, Key.Comma)) {
     override fun execute() {
         EchoInMirror.windowManager.dialogs[SettingsWindow] = true
     }
 }
 
-object PlayOrPauseCommand: AbstractCommand("EIM:Play or Pause", arrayOf(Key.Spacebar)) {
+object PlayOrPauseCommand : AbstractCommand("EIM:Play or Pause", "暂停/播放", arrayOf(Key.Spacebar)) {
     override fun execute() {
         EchoInMirror.currentPosition.isPlaying = !EchoInMirror.currentPosition.isPlaying
     }
