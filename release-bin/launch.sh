e150408484723760fcb5a86eb429f139ad419f64
Shirasawa <764798966@qq.com>
2023-02-09 17:16:02 +0800

    Update

--- release-bin/launch.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-java -Xms128m -Xmx4G -XX:ReservedCodeCacheSize=512m -XX:+UseZGC -XX:SoftRefLRUPolicyMSPerMB=50 -XX:CICompilerCount=2 -XX:-OmitStackTraceInFastThrow -XX:CompileCommand=exclude,com/intellij/openapi/vfs/impl/FilePartNodeRoot,trieDescend -ea -Dsun.io.useCanonCaches=false -Dsun.java2d.metal=true -Dsun.java2d.metal.displaySync=false -Djbr.catch.SIGABRT=true -Djdk.attach.allowAttachSelf=true -Djdk.module.illegalAccess.silent=true -Dkotlinx.coroutines.debug=off -jar EchoInMirror.jar
