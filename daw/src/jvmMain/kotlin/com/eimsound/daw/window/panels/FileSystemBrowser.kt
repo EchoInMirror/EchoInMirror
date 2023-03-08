8d0c76ac8c431df6647fa4b725420a5b71f44456
ClanEver <562211524@qq.com>
2023-03-08 22:10:04 +0800

    what is this?

--- daw/src/jvmMain/kotlin/com/eimsound/daw/window/panels/FileSystemBrowser.kt
+++ daw/src/jvmMain/kotlin/com/eimsound/daw/window/panels/FileSystemBrowser.kt
@@ -1,45 +1,27 @@
 package com.eimsound.daw.window.panels
 
-import androidx.compose.foundation.ExperimentalFoundationApi
-import androidx.compose.foundation.PointerMatcher
-import androidx.compose.foundation.background
-import androidx.compose.foundation.gestures.DraggableState
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.detectTapGestures
-import androidx.compose.foundation.gestures.draggable
-import androidx.compose.foundation.hoverable
+import androidx.compose.foundation.*
 import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.interaction.collectIsHoveredAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.FolderOpen
-import androidx.compose.material.icons.outlined.CheckBox
-import androidx.compose.material.icons.outlined.CheckBoxOutlineBlank
+import androidx.compose.material.icons.filled.*
+import androidx.compose.material.icons.outlined.DevicesOther
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.input.pointer.PointerButton
-import androidx.compose.ui.input.pointer.PointerIconDefaults
-import androidx.compose.ui.input.pointer.pointerHoverIcon
-import androidx.compose.ui.input.pointer.pointerInput
-import androidx.compose.ui.layout.onGloballyPositioned
 import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import cafe.adriel.bonsai.core.node.Node
-import cafe.adriel.bonsai.filesystem.FileSystemTree
 import com.eimsound.audioprocessor.AudioProcessorManager
-import com.eimsound.audioprocessor.AudioSourceManager
 import com.eimsound.audioprocessor.data.midi.*
-import com.eimsound.daw.EchoInMirror
 import com.eimsound.daw.api.window.Panel
 import com.eimsound.daw.api.window.PanelDirection
 import com.eimsound.daw.components.*
 import com.eimsound.daw.components.dragdrop.FileDraggable
-import com.eimsound.daw.components.utils.HorizontalResize
 import com.eimsound.daw.impl.processor.eimAudioProcessorFactory
 import com.eimsound.daw.processor.PreviewerAudioProcessor
 import kotlinx.coroutines.*
@@ -47,8 +29,7 @@ import okio.FileSystem
 import okio.Path
 import org.apache.commons.lang3.SystemUtils
 import java.io.File
-import javax.sound.midi.MidiSystem
-import kotlin.io.path.name
+import com.eimsound.daw.components.IconButton as EIMIconButton
 
 val FileMapper = @Composable { node: Node<Path>, content: @Composable () -> Unit ->
     if (FileSystem.SYSTEM.metadata(node.content).isDirectory) content()
@@ -57,7 +38,73 @@ val FileMapper = @Composable { node: Node<Path>, content: @Composable () -> Unit
 
 val fileBrowserPreviewer = PreviewerAudioProcessor(AudioProcessorManager.instance.eimAudioProcessorFactory)
 
-object FileSystemBrowser: Panel {
+@Composable
+fun FileNode(file: File, indent: Int = 0) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier.padding(start = (10 * (indent + 1)).dp)
+    ) {
+        Icon(
+            FileExtensionIcons.getOrDefault(file.extension, Icons.Outlined.DevicesOther),
+            file.name,
+            modifier = Modifier.size(24.dp, 24.dp)
+        )
+        Text(file.name, modifier = Modifier.padding(start = 4.dp))
+    }
+}
+
+@Composable
+fun FileTree(file: File, indent: Int = 0) {
+    var expanded by remember { mutableStateOf(false) }
+    Column {
+        if (file.isDirectory) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(start = (10 * (indent + 1)).dp)
+            ) {
+                Icon(
+                    Icons.Filled.Folder,
+                    contentDescription = null,
+                    modifier = Modifier.size(24.dp, 24.dp),
+                )
+                Text(file.name, modifier = Modifier.padding(start = 4.dp))
+//                Spacer(modifier = Modifier.weight(1f))
+                if (file.listFiles()!!.isNotEmpty()) {
+                    EIMIconButton(
+                        onClick = { expanded = !expanded },
+                        modifier = Modifier.padding(end = 4.dp)
+                    ) {
+                        Icon(
+                            if (expanded) Icons.Filled.ExpandLess else Icons.Filled.ExpandMore,
+                            contentDescription = null
+                        )
+                    }
+                }
+            }
+
+            if (expanded) {
+                for (child in file.listFiles()!!.sorted().sortedBy { it.isFile }) {
+                    FileTree(child, indent + 1)
+                }
+            }
+        } else FileNode(file, indent)
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FileTreeUI(filePath: File) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .verticalScroll(rememberScrollState())
+            .horizontalScroll(rememberScrollState())
+    ) {
+        FileTree(filePath)
+    }
+}
+
+object FileSystemBrowser : Panel {
     override val name = "文件浏览"
     override val direction = PanelDirection.Vertical
     private val filePath = when {
@@ -76,100 +123,7 @@ object FileSystemBrowser: Panel {
     @Composable
     override fun Content() {
         Column {
-            var component by remember { mutableStateOf<(@Composable BoxScope.() -> Unit)?>(null) }
-            var nodeName by remember { mutableStateOf<String?>(null) }
-            Tree(FileSystemTree(File(filePath), true), FileSystemStyle, FileMapper, Modifier.weight(1F)) {
-                if (FileSystem.SYSTEM.metadata(it.content).isDirectory) return@Tree
-                component = null
-                nodeName = null
-                val ext = it.content.toFile().extension.lowercase()
-                GlobalScope.launch {
-                    var hasContent = false
-                    try {
-                        if (ext == "mid") {
-                            val notes = withContext(Dispatchers.IO) {
-                                MidiSystem.getSequence(it.content.toFile()).toMidiTracks().getNotes()
-                            }
-                            component = { MidiView(notes) }
-                            nodeName = it.content.name
-                            fileBrowserPreviewer.setPreviewTarget(notes)
-                            hasContent = true
-                        } else if (AudioSourceManager.instance.supportedFormats.contains(ext)) {
-                            val file = it.content.toNioPath()
-                            val audioSource = AudioSourceManager.instance.createAudioSource(file)
-                            EchoInMirror.audioThumbnailCache[file, audioSource]?.let {
-                                component = { Waveform(it) }
-                                nodeName = file.name
-                                hasContent = true
-                            }
-                            fileBrowserPreviewer.setPreviewTarget(audioSource)
-                        }
-                    } catch (e: Exception) {
-                        hasContent = false
-                        e.printStackTrace()
-                    }
-                    if (!hasContent) {
-                        component = null
-                        nodeName = null
-                        fileBrowserPreviewer.clear()
-                    }
-                }
-            }
-            val width = remember { intArrayOf(1) }
-            Surface(Modifier.fillMaxWidth().height(40.dp).onGloballyPositioned { width[0] = it.size.width }, tonalElevation = 3.dp) {
-                @OptIn(ExperimentalFoundationApi::class)
-                DropdownMenu({ close ->
-                    MenuItem({
-                        fileBrowserPreviewer.position.isPlaying = !fileBrowserPreviewer.position.isPlaying
-                        close()
-                    }) {
-                        Text("自动播放")
-                        Filled()
-                        Icon(if (fileBrowserPreviewer.position.isPlaying) Icons.Outlined.CheckBox
-                        else Icons.Outlined.CheckBoxOutlineBlank, "自动播放")
-                    }
-                    MenuItem({
-                        fileBrowserPreviewer.position.isProjectLooping = !fileBrowserPreviewer.position.isProjectLooping
-                        close()
-                    }) {
-                        Text("循环播放")
-                        Filled()
-                        Icon(if (fileBrowserPreviewer.position.isProjectLooping) Icons.Outlined.CheckBox
-                        else Icons.Outlined.CheckBoxOutlineBlank, "自动播放")
-                    }
-                    MenuItem {
-                        Text("音量")
-                        Filled()
-                        Slider(fileBrowserPreviewer.volume, { fileBrowserPreviewer.volume = it }, Modifier.width(140.dp))
-                    }
-                    Divider()
-                    MenuItem(close) {
-                        Text(nodeName ?: "请选择文件...", overflow = TextOverflow.Ellipsis, maxLines = 1)
-                    }
-                }, enabled = false, matcher = PointerMatcher.mouse(PointerButton.Secondary)) {
-                    val c = component
-                    if (c == null) Box(Modifier.fillMaxSize(), Alignment.Center) {
-                        Text("请选择文件...", style = MaterialTheme.typography.labelMedium)
-                    } else Box {
-                        val draggableState = remember {
-                            DraggableState { fileBrowserPreviewer.playPosition += it / width[0].toDouble() }
-                        }
-                        val interactionSource = remember { MutableInteractionSource() }
-                        Box(Modifier.padding(horizontal = 4.dp), content = c)
-                        Box(Modifier.fillMaxSize()
-                            .draggable(draggableState, Orientation.Horizontal, interactionSource = interactionSource)
-                            .pointerInput(Unit) {
-                                detectTapGestures({
-                                    fileBrowserPreviewer.position.isProjectLooping = !fileBrowserPreviewer.position.isProjectLooping
-                                }) { fileBrowserPreviewer.playPosition = it.x / width[0].toDouble() }
-                            }
-                            .pointerHoverIcon(PointerIconDefaults.HorizontalResize)
-                        ) {
-                            PlayHead(interactionSource, width)
-                        }
-                    }
-                }
-            }
+            FileTreeUI(File(filePath))
         }
     }
 
@@ -182,10 +136,11 @@ object FileSystemBrowser: Panel {
             left.toDp()
         }
         val color = MaterialTheme.colorScheme.primary
-        Spacer(Modifier.width(if (isHovered) 4.dp else 2.dp).fillMaxHeight()
-            .graphicsLayer(translationX = left)
-            .hoverable(interactionSource)
-            .background(color)
+        Spacer(
+            Modifier.width(if (isHovered) 4.dp else 2.dp).fillMaxHeight()
+                .graphicsLayer(translationX = left)
+                .hoverable(interactionSource)
+                .background(color)
         )
         Spacer(Modifier.width(leftDp).fillMaxHeight().background(color.copy(0.14F)))
     }
